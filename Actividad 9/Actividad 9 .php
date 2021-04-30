66e8629cb5a85be4c882a98c36ce0798c5394496
Mel <melinatarchundiat@gmail.com>
2021-04-29 19:07:11 -0500

    FEAT agregué la actividad 9

---
+++ Actividad 9/Actividad 9 .php
@@ -0,0 +1,35 @@
+<?php
+    
+    echo "<table border=1>";
+        //dibuja las filas
+        $x=10;
+        for($y=1; $y<=$x; $y++)
+        {
+            echo "<tr>";
+            //dibuja columnas
+            for($z=1; $z<=$x; $z++)
+            {
+                echo '<td>';
+                //para que las filas que sean un número par comiencen con el negro
+                if($y%2==0)
+                {
+                    if($z%2==0)
+                        echo '<img src="./blanco.jpg" width="40" height="40">'; 
+                    else
+                        echo '<img src="./negro.jpg" width="40" height="40">';
+                }  
+                else
+                //para que las filas que sean un número impar comiencen con el blanco
+                {
+                    if($z%2!=0)
+                        echo '<img src="./blanco.jpg" width="40" height="40">'; 
+                    else
+                    echo '<img src="./negro.jpg" width="40" height="40">'; 
+                }
+                
+                echo '</td>';
+            }      
+            echo "</tr>";
+        }
+    echo "</table>";
+?>
